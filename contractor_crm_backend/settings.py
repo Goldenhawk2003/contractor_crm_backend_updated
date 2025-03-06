509e298833f156537eb18e5f5dc5d4066895e88c
Ammar Ogeil <ammarogeil@gmail.com>
2025-03-06 12:59:45 -0500

    Update settings.py

--- contractor_crm_backend/settings.py
+++ contractor_crm_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-&dse1c2s@8zio8t5^)lsy$af^*8(+#@h^#eptk^e3xva=#xqk*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'b9d7-216-249-49-34.ngrok-free.app', '6b1f-216-249-49-34.ngrok-free.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'b9d7-216-249-49-34.ngrok-free.app', '6b1f-216-249-49-34.ngrok-free.app', 'ecc-backend-31b43c38f51f.herokuapp.com']
 
 
 AUTH_USER_MODEL = 'contractors.User'  # Replace 'your_app' with the app where User is defined
